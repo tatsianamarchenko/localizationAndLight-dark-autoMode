943b2cc11ef56a08a4e1b42da939e48717aba2c0
Tatsiana <tanya3308225@mail.ru>
2022-02-02 15:47:27 +0300

    SUPPORTED localization

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   var languagesArray = ["English", "Беларуская", "Русский"]
-
+  
   var greeting : UILabel = {
     var text = UILabel()
     text.font = .boldSystemFont(ofSize: 20)
@@ -19,36 +19,34 @@ class ViewController: UIViewController {
     text.text = "Выберите язык приветствия"
     return text
   }()
-
+  
   var image : UIImageView = {
     var image = UIImageView()
     image.image = UIImage(named: "baseline_pets")
     return image
   }()
-
+  
   var lenguagePicker : UIPickerView = {
     var picker = UIPickerView()
     return picker
   }()
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     view.backgroundColor = .systemBackground
-
-    lenguagePicker.dataSource = self
-    lenguagePicker.delegate = self
     //
     greeting.frame = CGRect(x: 10, y: 10, width: 100, height: 100)
     image.frame = CGRect(x: 100, y: 300, width: 300, height: 300)
     lenguagePicker.frame = CGRect(x: 10, y: 200, width: 100, height: 100)
     //
+    lenguagePicker.dataSource = self
+    lenguagePicker.delegate = self
+    
     view.addSubview(greeting)
     view.addSubview(image)
     view.addSubview(lenguagePicker)
   }
-
-
 }
 
 
@@ -56,12 +54,22 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
     1
   }
-
+  
   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
     3
   }
-
+  
   func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
     return languagesArray[row]
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    switch row {
+    case 0 :   greeting.text = "Hello, Good morning, Good afternoon, Good evening, It is nice to meet you, It is a pleasure to meet you"
+    case 1 :   greeting.text = "Прывет, Добрай раніцы, Добры дзень, Добрага вечара, Добрай ночы!"
+    case 2 :   greeting.text = "Здравствуйте, Приветствую, Добрый день, Доброе утро, Добрый вечер"
+    default:
+      break
+    }
   }
 }
